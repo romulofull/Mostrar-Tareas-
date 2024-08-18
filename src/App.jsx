276d9d38791e099920ca731c388cafc32fd40fa4
romulofull <romulo.castro@gmail.com>
2024-08-17 22:13:33 -0500

    hecho

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+
+import TaskList from './TaskList'
+function App() {
+return (
+<div>   <TaskList/> </div>)
+
+}
+export default App
---
+++ src/tasks.js
@@ -0,0 +1,5 @@
+export const tasks = [
+  { id: 0, title: "mi primer tarea", description: "Mi primer Tarea" },
+  { id: 1, title: "mi segunda tarea", description: "Mi segunda Tarea" },
+  { id: 2, title: "mi tercera tarea", description: "Mi tercera Tarea" },
+];
